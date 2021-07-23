8e306554e63bd658555875596aaeb820c15eae79
NTBG\Sittiporn.Lek <sittiporn.lek@ntb.co.th>
2021-07-23 17:45:52 +0700

    add TennisGame1

---
+++ hello-test/src/main/java/TennisGame1.java
@@ -0,0 +1,38 @@
+public class TennisGame1 {
+    private int m_score1 = 0;
+    private int m_score2 = 0;
+    private final String player1Name;
+    private final String player2Name;
+
+    public TennisGame1(String player1Name, String player2Name) {
+        this.player1Name = player1Name;
+        this.player2Name = player2Name;
+    }
+
+    public void wonPoint(String playerName) {
+        if (this.player1Name.equals(playerName)) {
+            m_score1++;
+        } else {
+            m_score2++;
+        }
+    }
+
+    public String getScore() {
+        String[] scoreResults = new String[]{"Love", "Fifteen", "Thirty", "Forty" };
+        if (m_score1 == m_score2 && m_score1 <= 2) {
+            return scoreResults[m_score1] + "-All";
+        }
+        else if (m_score1 == m_score2 && m_score1 > 2) {
+            return "Deuce";
+        }
+        else if (m_score1 >= 4 || m_score2 >= 4) {
+            String playerWin = this.player1Name;
+            if (m_score1 < m_score2) playerWin = this.player2Name;
+            int minusResult = Math.abs(m_score1 - m_score2);
+            if (minusResult < 2) return "Advantage " + playerWin;
+            else return "Win for " + playerWin;
+        } else {
+            return scoreResults[m_score1] + "-" + scoreResults[m_score2];
+        }
+    }
+}
