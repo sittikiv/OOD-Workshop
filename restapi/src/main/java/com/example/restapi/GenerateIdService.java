d0550c0ed3a6ad33a1e45d4e983cce89fe5e75f0
NTBG\Sittiporn.Lek <sittiporn.lek@ntb.co.th>
2021-07-24 14:47:46 +0700

    add generateService

---
+++ restapi/src/main/java/com/example/restapi/GenerateIdService.java
@@ -0,0 +1,10 @@
+package com.example.restapi;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class GenerateIdService {
+    public String getId() {
+        return "XXX1";
+    }
+}
